1cf56ec0606a0928f4f9473417532b8b34111c4e
xxing2003 <xingxuemin2003@163.com>
2020-12-17 21:41:01 -0500

    add config files:config.py,config_default.py,config_override.py

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'xxing'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'Tellabs01@',
+        'db': 'moe'
+    },
+    'session': {
+        'secret': 'moe'
+    }
+}
