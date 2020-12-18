124e954444a87ecb904dedfaa5ded14e5205caa0
xxing2003 <xingxuemin2003@163.com>
2020-12-17 21:57:28 -0500

    modify files:app.py,handlers.py, add templates file

--- www/app.py
+++ www/app.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
             logger_factory, response_factory
         ])
         init_jinja2(app, filters=dict(datetime=datetime_filter))
-        add_routes(app, 'handler')
+        add_routes(app, 'handlers')
         add_static(app)
         srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
         logging.info('server started at http://127.0.0.1:9000...')
