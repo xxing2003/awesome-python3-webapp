1770417384aed53d1bb66691cf196b877aa3c826
xxing2003 <xingxuemin2003@163.com>
2020-12-17 21:14:35 -0500

    modify app.py, add apis.py, coroweb.py, handlers.py, test_view.py files

---
+++ www/test_view.py
@@ -0,0 +1,10 @@
+from coroweb import get
+import asyncio
+ 
+@get('/')
+async def index(request):
+	return '<h1>Awesome</h1>'
+ 
+@get('/hello')
+async def hello(request):
+	return '<h1>hello!</h1>'
