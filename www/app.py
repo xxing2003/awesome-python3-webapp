0d0744944bd9b4121aca7d95eb5d6b7ef42e96a1
xxing2003 <xingxuemin2003@163.com>
2020-12-18 01:44:58 -0500

    modify app.py

--- www/app.py
+++ www/app.py
@@ -48,6 +48,23 @@ async def logger_factory(app, handler):
         return (await handler(request))
     return logger
 
+@asyncio.coroutine
+def auth_factory(app, handler):
+    @asyncio.coroutine
+    def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = yield from cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (yield from handler(request))
+    return auth
+
 async def data_factory(app, handler):
     async def parse_data(request):
         if request.method == 'POST':
